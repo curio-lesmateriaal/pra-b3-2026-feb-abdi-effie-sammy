8e0a7ba88327b37d676b14be3dea1208391b9294
Effie541 <d311190@edu.curio.nl>
2026-03-12 09:52:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,29 @@
 <!doctype html>
 <html lang="nl">
-
+ 
 <head>
     <title></title>
     <?php require_once 'head.php'; ?>
 </head>
-
+ 
 <body>
-    
+    <?php
+        require_once "db.php";
+    ?>
     <div class="container">
-
-
+       
+    <a href=" --?delete=<?php echo $taak['id']; ?>">Verwijder</a>
+    <?php
+    if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    mysqli_query($conn, "DELETE FROM taken WHERE id=$id");
+}
+    ?>
+ 
     </div>
-
+ 
 </body>
-
+ 
 </html>
+ 
+ 
